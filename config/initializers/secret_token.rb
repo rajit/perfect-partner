213c6484e72f35d50e3104f2de9e699b21cdc821
Rajit Singh <rajisingh@ebay.com>
2012-05-21 06:26:18 +0100

    Create user-based system from template.  Add Facebook Omniauth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PerfectPartner::Application.config.secret_token = '6c041f2489efd12d9ec02dea0c0b3a169a0679c423f9cfd91fd0cf837de309672532776820c64af5e0ee46baf984be5c98deea24ce3950e88bf733df7eeee49b'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+PerfectPartner::Application.routes.draw do
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
+  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
+  resources :users, :only => [:show, :index]
+end
