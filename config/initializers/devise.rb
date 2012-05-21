6ae7d7f7876207543c0a814c0536f9345faf3fc8
Rajit Singh <rajisingh@ebay.com>
2012-05-21 08:16:03 +0100

    Use environment variables to configure facebook details

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -224,7 +224,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, '407123839310872', '3a0e1b27356ae46c81f7cd0cba88328a', :scope => 'email'
+  config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], :scope => 'email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
